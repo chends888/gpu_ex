e88a2034cfc075a44c9df6dc59a2b0fbba032fe5
EC2 Default User <ec2-user@ip-172-31-42-14.ec2.internal>
2019-11-01 12:39:03 +0000

    Proj3 WIP

--- proj3/main.cu
+++ proj3/main.cu
@@ -4,7 +4,10 @@
 #include <stdio.h>
 #include <math.h>
 
-#define SIZE 100000
+#include "curand.h"
+#include "curand_kernel.h"
+
+#define SIZE 1
 
 __global__ void calc_dists(double *xpos, double *ypos, double *dists, int N) {
     //printf("%d", N);
@@ -13,18 +16,37 @@ __global__ void calc_dists(double *xpos, double *ypos, double *dists, int N) {
     int j = blockIdx.x * blockDim.x + threadIdx.x;
     //printf("%d, %d \n", i, j);
     dists[i*N+j] = sqrt(pow((xpos[i] - xpos[j]), 2) + pow((ypos[i] - ypos[j]), 2));
-    printf("%f ", dists[i*N+j]);
+    //printf("%f ", dists[i*N+j]);
 }
 
-__global__ void shuff_vecs(double *) {
-    
+__global__ void shuff_vecs(double *xpos, double *ypos, int *points, int N) {
+    int t_i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    curandState st;
+    curand_init(0, 1, 0, &st);
+    //N = 4;
+
+    for (int i=1; i<N; i++) {
+        double tmp = points[i];
+        //double tmp_y = points[i];
+        //double new_x = xpos[(int) ((N-i) * curand_uniform(&st) + i)];
+        int rand_i = (int) ((N-i) * curand_uniform(&st) + i);
+        //posx[i] = posx[rand_i];
+        points[i] = points[rand_i];
+        points[rand_i] = tmp;
+        //posy[rand_i] = tmp_y;
+        //printf("%d, ", test);
+    }
+    for (int i=0; i<N; i++) {
+        printf("%d, ", points[i]);
+    }
 }
 
 int main() {
     int N;
     std::cin >> N;
 
-    thrust::host_vector<double> xpos(N), ypos(N), dists(N * N);
+    thrust::host_vector<double> xpos(N), ypos(N), dists(N * N), points(N);
     double x, y;
 
     for (int i=0; i<N; i++) {
@@ -32,6 +54,7 @@ int main() {
         std::cin >> y;
         xpos[i] = x;
         ypos[i] = y;
+        points[i] = i;
     }
     //for (int i=0; i<N; i++) {
     //    std::cout << xpos[i] << " ";
@@ -46,6 +69,7 @@ int main() {
     //cudaMalloc((void **) &ypos, sizeof(double) * N);
     //cudaMalloc((void **) &dists, sizeof(double) * N * N);
     thrust::device_vector<double> xpos_d(xpos), ypos_d(ypos), dists_d(dists);
+    thrust::device_vector<int> points_d(points);
 
     dim3 threads(32, 32);
     dim3 grid(N / threads.x, N / threads.y);
@@ -53,9 +77,9 @@ int main() {
                                   thrust::raw_pointer_cast(ypos_d.data()),
                                   thrust::raw_pointer_cast(dists_d.data()),
                                   N);
-    shuff_vecs<<<SIZE, 1024>>>(thrust::raw_pointer_cast(xpos_d.data()),
+    shuff_vecs<<<SIZE, 1>>>(thrust::raw_pointer_cast(xpos_d.data()),
                                thrust::raw_pointer_cast(ypos_d.data()),
-                               //thrust::raw_pointer_cast(dists_d.data()),
+                               thrust::raw_pointer_cast(points_d.data()),
                                N);
     //dim3 threads(1024);
     //dim3 grid(SIZE);
